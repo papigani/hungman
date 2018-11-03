a638a4abf24c190277d99a9f7876bcd07bf80649
unknown <pinvillhidemat@gmail.com>
2018-11-04 02:16:23 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 19:10:05 2018
+
+@author: matsu
+"""
+
+def hangman(word):
+    wrong=0
+    stages=["",
+            "________        ",
+            "|               ",
+            "|        |       ",
+            "|        O       ",
+            "|       /|\      ",
+            "|       / \      ",
+            "|                "
+            ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ！")
+    
+    while wrong < len(stages)-1:
+        print("\n")
+        msg="1文字予想してね："
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong +=1
+        
+        print(" ".join(board))
+        endnum= wrong+1
+        print("\n".join(stages[0:endnum]))
+        if "_" not in board:
+            print("あなたの勝ち！")
+            print(" ".join(board))
+            win = True
+            break
+    
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け！正解は　{}".format(word))
+
+hangman("cat")
